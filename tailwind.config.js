907b0a32a811e1a92445dfff320bbf4365809a01
mounikagollu1996 <mounikagollu1996@gmail.com>
2020-09-14 20:27:41 +0530

    Add new line heights

--- tailwind.config.js
+++ tailwind.config.js
@@ -295,14 +295,16 @@ module.exports = {
       normal: '1.5',
       relaxed: '1.625',
       loose: '2',
-      3: '.75rem',
+      3: '.875rem',
       4: '1rem',
-      5: '1.25rem',
-      6: '1.5rem',
-      7: '1.75rem',
-      8: '2rem',
-      9: '2.25rem',
-      10: '2.5rem'
+      5: '1.125rem',
+      6: '1.25rem',
+      7: '1.375rem',
+      8: '1.5rem',
+      9: '1.75rem',
+      10: '2rem',
+      11: '3.5rem',
+      12: '5rem'
     },
     listStyleType: {
       none: 'none',
