039efcb348e710c4c146b3e366e2646bd12835b9
Sanket Saurav <sanket@deepsource.io>
2020-09-16 17:57:28 +0530

    Fix #6: Change borderRadius property

--- tailwind.config.js
+++ tailwind.config.js
@@ -176,9 +176,8 @@ module.exports = {
     borderRadius: {
       none: '0',
       sm: '0.125rem',
-      default: '0.25rem',
-      md: '0.375rem',
-      lg: '0.5rem',
+      md: '0.25rem',
+      lg: '0.375rem',
       full: '9999px'
     },
     borderWidth: {
