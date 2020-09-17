6518fd82fadd24649df1714f0b9f86718dadbcb0
mounikagollu1996 <mounikagollu1996@gmail.com>
2020-09-17 20:21:44 +0530

    Add default button changes

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-    Button
+  <button class="bg-juniper text-ink-400 leading-8 font-normal py-2 px-4 rounded-sm">
+    Default button
   </button>
 </template>
 
