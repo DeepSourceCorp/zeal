07de69581eb0b8319b8293d3c6b7a42df34d2c5d
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2020-12-10 11:20:41 +0530

    Fix the import issues in Zeal

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import './assets/css/tailwind.css'
 import './assets/css/typography.css'
 // import App from './App.vue';
-import { ZButton } from './components/ZButton/index'
+import  ZButton  from './components/ZButton/index'
 
 const components = [ZButton]
 
