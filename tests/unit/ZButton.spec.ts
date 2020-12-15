c7942a97be5025d470b65e89c2188aac2be480f0
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2020-12-15 11:00:44 +0530

    Add unit tests for Button Component

---
+++ tests/unit/ZButton.spec.ts
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import { createLocalVue, mount, Wrapper, shallowMount } from '@vue/test-utils';
+
+import ZButton from '../../src/components/ZButton';
+
+describe('Button component', () => {
+    let mountFunction: (options?: object) => Wrapper<Vue>
+    let localVue: typeof Vue
+    beforeEach(() => {
+        localVue = createLocalVue()
+        mountFunction = (options = {}) => {
+            return mount(ZButton, {
+                localVue, 
+                ...options
+            })
+        }
+    })
+    it('renders a button tag when the component is mounted', () => {
+        expect(mountFunction().html()).toMatch('<button type="button" class="z-btn z-btn--primary">Click</button>')
+    })
+})
