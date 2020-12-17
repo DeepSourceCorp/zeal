8d3005b5e7fdae772f359edea8dd7bd0e386d3a2
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2020-12-17 14:15:54 +0530

    Address review comments

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,6 @@ import { configure } from '@storybook/vue'
 import '../src/assets/css/tailwind.css'
 import '../src/assets/css/typography.css'
 import '../src/assets/css/layout.css'
-import '../src/assets/css/story.css'
+import './css/story.css';
 
 configure(require.context('../src', true, /\.stories\.js$/), module)
