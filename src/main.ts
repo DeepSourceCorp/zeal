96a962594926743956ea6701997baa641eb45374
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2020-12-23 01:13:25 +0530

    Fix the import statements in main file

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import './assets/css/layout.css'
 
 import ZButton from './components/ZButton/index'
 import ZIcon from './components/ZIcon/index'
-import ZDivider from './components/ZIcon/index'
-import { ZCarousel, ZSlide } from './components/ZCarousel/index'
-import ZCard from './components/ZIcon/index'
+import ZDivider from './components/ZDivider/index'
+import ZCarousel from './components/ZCarousel/index'
+import ZSlide from './components/ZSlide/index'
+import ZCard from './components/ZCard/index'
 import '../dist/tailwind.css'
 
 const components = [ZButton, ZIcon, ZDivider, ZCarousel, ZSlide, ZCard]
@@ -26,5 +27,6 @@ if (typeof window !== 'undefined' && window.Vue) {
 export { default as ZButton } from './components/ZButton/index'
 export { default as ZIcon } from './components/ZIcon/index'
 export { default as ZDivider } from './components/ZDivider/index'
-export { ZCarousel, ZSlide } from './components/ZCarousel/index'
+export { default as ZCarousel } from './components/ZCarousel/index'
+export { default as ZSlide } from './components/ZSlide/index'
 export { default as ZCard } from './components/ZCard/index'
