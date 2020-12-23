81a35368986b83f785ed35bdced8ce4be7c103a8
Aman Sharma <aman@deepsource.io>
2020-12-23 14:01:16 +0530

    fix: WIP to make css reflect in storybook on changes in components

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,8 @@
 import { configure } from '@storybook/vue'
 
-import '../src/assets/css/tailwind.css'
-import '../src/assets/css/typography.css'
-import '../src/assets/css/layout.css'
+import '../dist/tailwind.css'
+import '../dist/typography.css'
+import '../dist/layout.css'
 import './css/story.css'
 
 configure(require.context('../src', true, /\.stories\.js$/), module)
