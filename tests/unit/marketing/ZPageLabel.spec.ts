d3967fc631d4f2327f8aff942f48f9172ee63ac6
Aman Sharma <aman@deepsource.io>
2020-12-25 00:21:39 +0530

    feat: Tests for PageLabel component

--- tests/unit/marketing/ZPageLabel.spec.ts
+++ tests/unit/marketing/ZPageLabel.spec.ts
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import { createLocalVue, mount, Wrapper, shallowMount } from '@vue/test-utils'
+import { createLocalVue, mount, Wrapper } from '@vue/test-utils'
 
 import ZPageLabel from '../../../src/components/marketing/ZPageLabel'
 
-describe('Button component', () => {
+describe('Page Label component', () => {
   let mountFunction: (options?: object) => Wrapper<Vue>
   let localVue: typeof Vue
   beforeEach(() => {
@@ -15,114 +15,61 @@ describe('Button component', () => {
       })
     }
   })
-  it('renders a button tag when the component is mounted', () => {
+  it('renders a page label when the component is mounted', () => {
     expect(mountFunction().html()).toMatchSnapshot()
   })
 
-  it('renders a button tag with the given text', () => {
-    expect(
-      mountFunction({
-        slots: {
-          default: 'Hello World'
-        }
-      }).html()
-    ).toMatchSnapshot()
+  it('renders a page label with the given text', () => {
+    let mountedInstance = mountFunction({
+      slots: {
+        default: 'Guide'
+      }
+    });
+    expect(mountedInstance.html()).toMatchSnapshot()
+    expect(mountedInstance.text()).toContain('Guide')
   })
 
-  it('renders a button with primary color', () => {
-    expect(
-      mountFunction({
-        propsData: {
-          color: 'primary'
-        },
-        slots: {
-          default: 'Primary Button'
-        }
-      }).html()
-    ).toMatchSnapshot()
+  it('renders a page label with default gradient background', () => {
+    let mountedInstance = mountFunction({
+      slots: {
+        default: 'Guide'
+      }
+    });
+    expect(mountedInstance.html()).toMatchSnapshot()
+    expect(mountedInstance.classes()).toContain('bg-gradient-dawn')
   })
-  it('renders a button with secondary color', () => {
-    expect(
-      mountFunction({
-        propsData: {
-          color: 'secondary'
-        },
-        slots: {
-          default: 'Secondary Button'
-        }
-      }).html()
-    ).toMatchSnapshot()
-  })
-  it('renders a button with the given size(large)', () => {
-    expect(
-      mountFunction({
-        propsData: {
-          size: 'large'
-        },
-        slots: {
-          default: 'Large Button'
-        }
-      }).html()
-    ).toMatchSnapshot()
-  })
-  it('renders a disabled button when the disabled attribute is passed', () => {
-    expect(
-      mountFunction({
-        propsData: {
-          disabled: true
-        },
-        slots: {
-          default: 'Disabled Button'
-        }
-      }).html()
-    ).toMatchSnapshot()
-  })
-  it('renders a full-width button', () => {
-    expect(
-      mountFunction({
-        propsData: {
-          fullWidth: true
-        },
-        slots: {
-          default: 'Full Width Button'
-        }
-      }).html()
-    ).toMatchSnapshot()
-  })
-  it('renders a link tag when type is specified as link', () => {
-    expect(
-      mountFunction({
-        propsData: {
-          type: 'link',
-          color: 'link',
-          to: 'https://deepsource.io/'
-        },
-        slots: {
-          default: 'Link Button'
-        }
-      }).html()
-    ).toMatchSnapshot()
-  })
-  it('should use custom classes', () => {
-    const wrapper = mountFunction({
+  it('renders a page label with custom background color', () => {
+    let mountedInstance = mountFunction({
       propsData: {
-        customClasses: 'bg-transparent'
+        backgroundColor: 'ink-400'
       },
       slots: {
-        default: 'Custom Styled Button'
+        default: 'Guide'
       }
-    })
-
-    expect(wrapper.classes('bg-transparent')).toBe(true)
+    });
+    expect(mountedInstance.html()).toMatchSnapshot()
+    expect(mountedInstance.classes()).toContain('bg-ink-400')
   })
-  it('button should emit a click event', () => {
-    const click = jest.fn()
-    const wrapper = mountFunction({
-      listeners: {
-        click: click
+
+  it('renders a page label with default text color', () => {
+    let mountedInstance = mountFunction({
+      slots: {
+        default: 'Guide'
       }
-    })
-    wrapper.find('.z-btn').trigger('click')
-    expect(click).toHaveBeenCalledTimes(1)
+    });
+    expect(mountedInstance.html()).toMatchSnapshot()
+    expect(mountedInstance.classes()).toContain('text-vanilla-100')
+  })
+  it('renders a page label with custom text color', () => {
+    let mountedInstance = mountFunction({
+      propsData: {
+        textColor: 'slate'
+      },
+      slots: {
+        default: 'Guide'
+      }
+    });
+    expect(mountedInstance.html()).toMatchSnapshot()
+    expect(mountedInstance.classes()).toContain('text-slate')
   })
 })
