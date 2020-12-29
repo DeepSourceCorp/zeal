2afaee9b25c02ea225bfec8ca3c009f623ba6340
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2020-12-29 17:06:21 +0530

    Address review comments

--- src/components/ZCheckbox/ZCheckbox.vue
+++ src/components/ZCheckbox/ZCheckbox.vue
@@ -1,8 +1,9 @@
 <template>
-  <label
-    class="z-input z-input__checkbox block relative pl-6 mb-1 text-base flex items-center"
-    :class="[`${disabled && `cursor-not-allowed`}`, { 'is-disabled': this.disabled !== false }]"
-  >
+  <label class="group z-input z-input__checkbox block relative pl-6 mb-1 text-base flex items-center" 
+        :class="[
+          `${disabled && `cursor-not-allowed`}`,
+          { 'is-disabled': this.disabled !== false }
+        ]">
     <input
       type="checkbox"
       :checked="isChecked"
@@ -11,16 +12,18 @@
       :disabled="this.disabled"
       class="absolute opacity-0 h-0 w-0"
     />
-    <span
-      class="z-input__checkbox--checkmark grid place-items-center absolute left-0 h-5 w-5 rounded-sm border border-slate border-solid bg-transparent cursor-pointer"
-      :class="[`${disabled && `cursor-not-allowed`}`]"
-    ></span>
-    <span class="z-input__checkbox--text ml-2" :class="[`${disabled && `text-slate`}`]">
+    <span data-before="✓" data-content='' class="z-input__checkbox--checkmark before:content-before text-vanilla-100 text-center text-lg sibling-checked:bg-juniper sibling-checked:border-juniper grid place-items-center absolute left-0 h-5 w-5 rounded-sm border border-slate border-solid bg-transparent cursor-pointer group-hover:border-juniper"
+          :class="[
+            `${disabled && `cursor-not-allowed group-hover:border-slate sibling-checked:text-ink-300 sibling-checked:border-slate sibling-checked:bg-slate`}`
+          ]"></span>
+    <span class="z-input__checkbox--text ml-2"
+          :class="[
+            `${disabled && `text-slate`}`
+          ]">
       {{ label }}
     </span>
   </label>
 </template>
-
 <script>
 export default {
   name: 'ZCheckbox',
@@ -81,45 +84,3 @@ export default {
   }
 }
 </script>
-
-<style lang="css" scoped>
-/* On mouse-over, add a grey background color */
-.z-input__checkbox:hover input ~ .z-input__checkbox--checkmark {
-  @apply border border-solid border-juniper;
-}
-
-/* When the checkbox is checked, add a blue background */
-.z-input__checkbox input:checked ~ .z-input__checkbox--checkmark {
-  @apply border border-solid border-juniper bg-juniper;
-}
-
-/* Create the checkmark/indicator (hidden when not checked) */
-.z-input__checkbox--checkmark:after {
-  @apply absolute hidden;
-}
-
-/* Show the checkmark when checked */
-.z-input__checkbox input:checked ~ .z-input__checkbox--checkmark:after {
-  @apply block;
-}
-
-/* Style the checkmark/indicator */
-.z-input__checkbox .z-input__checkbox--checkmark:after {
-  content: '\2713';
-  @apply text-vanilla-100 text-center text-lg;
-}
-
-/* Styles for Disabled Proeprty */
-
-.z-input__checkbox.is-disabled:hover .z-input__checkbox--checkmark {
-  @apply border-slate;
-}
-
-.z-input__checkbox.is-disabled input:checked ~ .z-input__checkbox--checkmark {
-  @apply border border-solid border-slate bg-slate;
-}
-
-.z-input__checkbox.is-disabled input:checked ~ .z-input__checkbox--checkmark:after {
-  @apply text-ink-300;
-}
-</style>
