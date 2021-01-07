6cabf273b9e0d8907aa9d61c3072e9ba55f7fb1b
Aman Sharma <aman@deepsource.io>
2021-01-07 19:03:10 +0530

    fix: Color of text

--- src/components/marketing/ZRelatedArticle/ZRelatedArticle.vue
+++ src/components/marketing/ZRelatedArticle/ZRelatedArticle.vue
@@ -1,6 +1,6 @@
 <template>
   <span class="grid grid-cols-12 py-3">
-    <span class="col-span-10 mb-2 block text-lg font-medium leading-9">
+    <span class="col-span-10 mb-2 block text-vanilla-200 text-lg font-medium leading-9">
       <slot name="heading"></slot>
     </span>
     <span class="flex items-center col-span-10 text-sm font-medium uppercase text-juniper">
