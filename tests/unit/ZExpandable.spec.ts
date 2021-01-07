ecb0f2651ba5f7c1b0686f528762d1b975ea90eb
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2021-01-07 21:24:37 +0530

    Add Spec files

---
+++ tests/unit/ZExpandable.spec.ts
@@ -0,0 +1,47 @@
+import ZExpandable from '../../src/components/ZExpandable'
+import Vue from 'vue'
+
+import { mount, Wrapper } from '@vue/test-utils'
+
+describe('ZExpandable', () => {
+  let mountFn: (options?: object) => Wrapper<Vue>
+  let localVue: typeof Vue
+
+  beforeEach(() => {
+    mountFn = (options = {}) => {
+      return mount(ZExpandable, {
+        localVue,
+        ...options
+      })
+    }
+  })
+
+  it('renders the default expandable component', () => {
+    const wrapper = mountFn({
+        slots: {
+            header: '<p>Non-iterable value used in an iterating context</p>',
+            content: ` <p>
+                    Consistent within interface: all elements should be consistent, such as: design style, icons and texts, position of elements, etc uch as: design style, icons and texts, position of elements, etc uch as: design style, icons and texts, position of elements, etc.
+                </p>`
+        }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.classes()).toContain('z-expandable')
+  })
+
+  it('renders an expandable in an open state', () => {
+    const wrapper = mountFn({
+      propsData: {
+        open: true
+        },
+        slots: {
+            header: '<p>Non-iterable value used in an iterating context</p>',
+            content: ` <p>
+                    Consistent within interface: all elements should be consistent, such as: design style, icons and texts, position of elements, etc uch as: design style, icons and texts, position of elements, etc uch as: design style, icons and texts, position of elements, etc.
+                </p>`
+        }
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
