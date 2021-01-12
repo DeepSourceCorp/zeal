2801dfcb52eaf0bb36b44388dde5840289f051ce
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2021-01-12 10:59:01 +0530

    Code cleanup

--- src/components/ZInput/ZInput.vue
+++ src/components/ZInput/ZInput.vue
@@ -1,24 +1,20 @@
 <template>
     <div class="z-input w-full relative">
-        <label for="input" v-if="label" 
-                class="block mb-1 text-xs"
-                :class="`${disabled && 'text-slate cursor-not-allowed' || 'text-vanilla-300'}`">{{label}}</label>
         <span v-if="icon && iconPosition === 'left'"
                 class="absolute h-full flex items-center top-0 text-center transition-all left-2 cursor-pointer">
             <z-icon :icon="icon" size="small"></z-icon>
         </span>
         <input type="text"
-            class="input w-full text-sm bg-ink-400 focus:shadow-white focus:border-vanilla-400 border border-solid border-bg-ink-100 rounded-sm outline-none h-10"
+            class="input w-full pt-2.5 text-sm bg-ink-400 focus:shadow-white focus:border-vanilla-400 border border-solid border-bg-ink-100 rounded-sm outline-none h-10"
             :class="[`${disabled && 'text-slate cursor-not-allowed' || 'text-vanilla-300'}`,
-                    `${(iconPosition && iconPosition === 'left') && 'pl-8 p-3' || 'p-3'}`,
-                    `${(iconPosition && iconPosition === 'right') && 'pr-8 p-3' || 'p-3'}`
+                    getIconBasedStyle
                    ]"
             :value="name"
             :placeholder="placeholder"
             :disabled="disabled"
             @input="updateSelf($event.target.value)">
-        <span v-if="clearable" 
-                class="absolute h-full flex items-center top-0 text-center transition-all right-2 cursor-pointer"
+        <span v-if="clearable && !icon" 
+                class="absolute h-full flex items-center top-0 text-center right-2 transition-all cursor-pointer"
                 @click.stop="updateSelf('')">
             <z-icon icon="x" size="small"></z-icon>
         </span>
@@ -46,9 +42,6 @@ export default {
         default: "Enter a value",
         type: String
     },
-    label: {
-        type: String
-    },
     disabled: {
         default: false,
         type: Boolean
@@ -61,12 +54,23 @@ export default {
     },
     iconPosition: {
         type: String,
+        default: "left"
     }
   },
   model: {
       prop: "name",
       event: "input"
   }, 
+  computed: {
+      getIconBasedStyle() {
+          const iconPositions = {
+              left: 'pl-8 p-3',
+              right: 'pr-8 p-3',
+              none: 'p-3'
+          }
+          return this.icon && iconPositions[this.iconPosition] || iconPositions["none"];
+      }
+  },
   methods: {
       updateSelf(name) {
           this.$emit("input", name);
