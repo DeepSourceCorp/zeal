35f47eaebff3675d4d52d879786572ff38ff7e49
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2021-01-13 13:34:32 +0530

    Add radio button implementation

---
+++ src/components/ZRadio/ZRadio.stories.ts
@@ -0,0 +1,66 @@
+
+import '../../assets/css/tailwind.css'
+import '../../assets/css/typography.css'
+import '../../assets/css/layout.css'
+import ZRadio from './ZRadio.vue'
+
+export default {
+  title: 'Form Inputs and Controls: Radio Button',
+  component: ZRadio,
+  excludeStories: /.*Data$/
+}
+
+export const DefaultRadio = () => ({
+    components: { ZRadio },
+    template: `<div class='padded-container'>
+            <div class="input-container">
+                <z-radio value="female" v-model="modelValue" label="Female"></z-radio>
+                <z-radio value="male" v-model="modelValue" label="Male"></z-radio>
+            </div>
+        </div>`
+})
+
+export const CheckedRadioButton = () => ({
+    components: { ZRadio },
+    data() {
+        return {
+            modelValue: "female"
+        }
+    },
+    template: `<div class='padded-container'>
+            <div class="input-container">
+                <z-radio value="female" v-model="modelValue" label="Female"></z-radio>
+                <z-radio value="male" v-model="modelValue" label="Male"></z-radio>
+            </div>
+        </div>`
+})
+
+export const DisabledRadioButton = () => ({
+    components: { ZRadio },
+    data() {
+        return {
+            modelValue: "female"
+        }
+    },
+    template: `<div class='padded-container'>
+            <div class="input-container">
+                <z-radio value="female" v-model="modelValue" label="Female"></z-radio>
+                <z-radio value="male" v-model="modelValue" label="Male" :disabled="true"></z-radio>
+            </div>
+        </div>`
+})
+
+export const DisabledCheckedRadioButton = () => ({
+    components: { ZRadio },
+    data() {
+        return {
+            modelValue: "female"
+        }
+    },
+    template: `<div class='padded-container'>
+            <div class="input-container">
+                <z-radio value="female" v-model="modelValue" label="Female"></z-radio>
+                <z-radio value="male" v-model="modelValue" label="Male"></z-radio>
+            </div>
+        </div>`
+})
---
+++ tests/unit/ZRadio.spec.ts
@@ -0,0 +1,65 @@
+import ZRadio from '../../src/components/ZRadio'
+import Vue from 'vue'
+
+import { mount, Wrapper } from '@vue/test-utils'
+
+describe('Radio Button Component', () => {
+  let mountFn: (options?: Record<string, unknown>) => Wrapper<Vue>
+  let localVue: typeof Vue
+
+  beforeEach(() => {
+    mountFn = (options = {}) => {
+      return mount(ZRadio, {
+        localVue,
+        ...options
+      })
+    }
+  })
+
+    it('renders the default radio component', () => {
+        const wrapper = mountFn({
+            propsData: {
+            label: "Hello"
+            }
+        })
+
+        expect(wrapper.html()).toMatchSnapshot()
+        expect(wrapper.findAll('input[type="radio"]').length).toEqual(1)
+    })
+    
+    it('renders the checked radio component', () => {
+        const wrapper = mountFn({
+            propsData: {
+                label: "Female",
+                modelValue: "female",
+                value: "female"
+            }
+        })
+        expect(wrapper.html()).toMatchSnapshot()
+        expect(wrapper.findAll('.z-radio--inner span').length).toEqual(1)
+    })
+    it('renders the disabled radio component', () => {
+        const wrapper = mountFn({
+            propsData: {
+                label: "Female",
+                value: "female",
+                disabled: true
+            }
+        })
+        expect(wrapper.html()).toMatchSnapshot()
+        expect(wrapper.findAll('input[disabled="disabled"]').length).toEqual(1)
+    })
+    it('renders a checked radio button disabled', () => {
+        const wrapper = mountFn({
+            propsData: {
+                label: "Female",
+                value: "female",
+                disabled: true,
+                modelValue: "female"
+            }
+        })
+        expect(wrapper.html()).toMatchSnapshot()
+        expect(wrapper.findAll('input[disabled="disabled"]').length).toEqual(1)
+        expect(wrapper.findAll('.z-radio--inner span').length).toEqual(1)
+    })
+})
