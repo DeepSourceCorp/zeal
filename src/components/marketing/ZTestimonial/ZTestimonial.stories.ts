982b59b3613475a5aab248b8cc1d8d2a8061cf5d
Aman Sharma <aman@deepsource.io>
2021-01-13 09:02:02 +0530

    fix: Add styling according to mock

--- src/components/marketing/ZTestimonial/ZTestimonial.stories.ts
+++ src/components/marketing/ZTestimonial/ZTestimonial.stories.ts
@@ -10,6 +10,9 @@ export const Basic = () => ({
   components: { ZTestimonial },
   template: `<div class='padded-container w-1/2'>
       <z-testimonial class="bg-ink-300">
+        <template slot="image">
+          <img src="https://i.imgur.com/ISsHpNY.png" class="object-contain"/>
+        </template>
         <template slot="body">
           DeepSource actually helped us resolve more than 50 bugs, which has made the code base much more stable and dependable. It allowed us to identify many more areas for improvement.
         </template>
--- src/components/marketing/ZTestimonial/ZTestimonial.vue
+++ src/components/marketing/ZTestimonial/ZTestimonial.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="rounded-xl overflow-hidden transition-all shadow flex flex-col h-full px-10 py-8">
-    <div class="text-vanilla-100 text-xl flex-1 mb-6 pr-2" v-if="$slots.body">
-      <slot name="body"> </slot>
+  <div class="rounded-xl overflow-hidden transition-all shadow flex flex-col justify-around h-full px-10 py-8">
+    <div class="flex-1" v-if="$slots.image">
+      <slot name="image"> </slot>
     </div>
-    <div class="text-vanilla-400 text-lg" v-if="$slots.footer">
-      <slot name="footer"> </slot>
-    </div>
+    <span class="flex-1">
+      <div class="text-vanilla-100 text-xl mb-6 pr-2" v-if="$slots.body">
+        <slot name="body"> </slot>
+      </div>
+      <div class="text-vanilla-400 text-lg" v-if="$slots.footer">
+        <slot name="footer"> </slot>
+      </div>
+    </span>
   </div>
 </template>
 
