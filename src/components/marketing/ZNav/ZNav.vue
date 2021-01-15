b3818ca0448ecab42c82fa88561ecfa1616690d2
Aman Sharma <aman@deepsource.io>
2021-01-15 14:50:31 +0530

    WIP: Navbar

---
+++ src/components/marketing/ZNav/ZNav.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="sticky top-0 backdrop-blur bg-ink-400 text-vanilla-100 h-16">
+    <!-- Container -->
+    <div class="grid grid-cols-3">
+      <div>
+        <slot name="logo">LOGO</slot>
+      </div>
+      <div>
+        <slot name="menu">Menu Items</slot>
+      </div>
+      <div>
+        <slot name="cta">CTAs</slot>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ZNav'
+})
+</script>
