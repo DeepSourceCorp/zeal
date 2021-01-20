9ed01793916876e29970e1eaa395003a4c78f437
Shivam Mishra <shivam@deepsource.io>
2021-01-20 16:09:30 +0530

    chore: update stories

--- src/components/ZTabs/ZTab.stories.ts
+++ src/components/ZTabs/ZTab.stories.ts
@@ -6,6 +6,7 @@ import ZTab from './ZTab.vue'
 import ZTabList from './ZTabList.vue'
 import ZTabItems from './ZTabItems.vue'
 import ZTabItem from './ZTabItem.vue'
+import ZIcon from '../ZIcon/ZIcon.vue'
 
 export default {
   title: 'Tabs',
@@ -18,14 +19,48 @@ export const BasicTabs = () => ({
   template: `<div class='padded-container'>
               <z-tabs>
                 <z-tab-list>
-                  <z-tab>Code</z-tab>
+                  <z-tab>Overview</z-tab>
                   <z-tab>Issues</z-tab>
-                  <z-tab>Pull Requests</z-tab>
+                  <z-tab>Metrics</z-tab>
+                  <z-tab>Autofix</z-tab>
                 </z-tab-list>
                 <z-tab-items>
-                  <z-tab-item>This is your code.</z-tab-item>
+                  <z-tab-item>Overview of the code.</z-tab-item>
                   <z-tab-item>There are the list of issues.</z-tab-item>
-                  <z-tab-item>All the Pull Requests</z-tab-item>
+                  <z-tab-item>This are metrics.</z-tab-item>
+                  <z-tab-item>Works like magic</z-tab-item>
+                </z-tab-items>
+              </z-tabs>
+      </div>`
+})
+
+export const TabsWithIcons = () => ({
+  components: { ZTabs, ZTabList, ZTab, ZTabItems, ZTabItem, ZIcon },
+  template: `<div class='padded-container'>
+              <z-tabs>
+                <z-tab-list class="bg-ink-300 p-4 pb-0">
+                  <z-tab>
+                    <z-icon icon="activity" size="small"></z-icon>
+                    Overview
+                  </z-tab>
+                  <z-tab>
+                    <z-icon icon="code" size="small"></z-icon>
+                    Issues
+                  </z-tab>
+                  <z-tab>
+                    <z-icon icon="bar-chart" size="small"></z-icon>
+                    Metrics
+                  </z-tab>
+                  <z-tab>
+                  <z-icon icon="settings" size="small"></z-icon>
+                    Settings
+                  </z-tab>
+                </z-tab-list>
+                <z-tab-items class="p-4">
+                  <z-tab-item>Overview of the code.</z-tab-item>
+                  <z-tab-item>There are the list of issues.</z-tab-item>
+                  <z-tab-item>This are metrics.</z-tab-item>
+                  <z-tab-item>Works like magic</z-tab-item>
                 </z-tab-items>
               </z-tabs>
       </div>`
