bd5027e590f10308d848d3b21fa40befe97c7d71
Shivam Mishra <shivam@deepsource.io>
2021-01-21 11:30:15 +0530

    refactor: better exports

--- src/components/ZTabs/index.ts
+++ src/components/ZTabs/index.ts
@@ -1,3 +1,9 @@
 import ZTabs from './ZTabs.vue'
+import ZTab from './ZTab.vue'
+import ZTabList from './ZTabList.vue'
+import ZTabItems from './ZTabItems.vue'
+import ZTabItem from './ZTabItem.vue'
+
+export { ZTabs, ZTabList, ZTab, ZTabItems, ZTabItem }
 
 export default ZTabs
