c20aab7061b5cc1402a8be7ba63c2cf5922bf5e6
Aman Sharma <aman@deepsource.io>
2021-01-21 12:29:42 +0530

    fix: Cleanup component. Remove clutter from tailwind.config
    Signed-off-by: Aman Sharma <aman@deepsource.io>

--- src/components/marketing/ZNavMenu/ZNavMenu.stories.ts
+++ src/components/marketing/ZNavMenu/ZNavMenu.stories.ts
@@ -9,6 +9,12 @@ export default {
 export const Basic = () => ({
   components: { ZNavMenu },
   template: `<div class='bg-padded-container px-40'>
-      <z-nav-menu></z-nav-menu>
+      <z-nav-menu title="Documentation">
+        <ul>
+          <li class="p-2">Link</li>
+          <li class="p-2">Link</li>
+          <li class="p-2">Link</li>
+        </ul>
+      </z-nav-menu>
     </div>`
 })
