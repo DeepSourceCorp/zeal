3576319bfbb592881d498c1488bbdd1c7252a29b
Shivam Mishra <shivam@deepsource.io>
2021-01-25 14:01:43 +0530

    refactor: use typescript

--- src/components/ZPulse/ZPulse.vue
+++ src/components/ZPulse/ZPulse.vue
@@ -1,13 +1,15 @@
 <template>
   <div :class="`flex flex-shrink-0 h-${pulseSize} w-${pulseSize} items-center justify-center`">
     <span
-      :class="`animate-pulse absolute inline-flex h-${pulseSize} w-${pulseSize} rounded-full bg-${color} bg-opacity-75`"
+      :class="
+        `animate-pulse absolute inline-flex h-${pulseSize} w-${pulseSize} rounded-full bg-${color} bg-opacity-75`
+      "
     ></span>
     <span :class="`relative inline-flex rounded-full h-${dotSize} w-${dotSize} bg-${color}`"></span>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
@@ -20,18 +22,18 @@ export default Vue.extend({
     size: {
       type: String,
       default: 'base',
-      validator: function (value) {
+      validator: function(value: string): boolean {
         return ['small', 'base', 'large'].includes(value)
       }
     }
   },
   computed: {
-    pulseSize() {
+    pulseSize(): string {
       if (this.size === 'small') return '3'
       else if (this.size === 'large') return '6'
       return '4'
     },
-    dotSize() {
+    dotSize(): string {
       if (this.size === 'small') return '1'
       else if (this.size === 'large') return '3'
       return '2'
