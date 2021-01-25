b20dc22a1248089b6fd87742cba0f505e63d5e31
Shivam Mishra <shivam@deepsource.io>
2021-01-25 12:43:53 +0530

    fix(style): add type annotations for loop fn

--- src/components/ZTabs/ZTabList.vue
+++ src/components/ZTabs/ZTabList.vue
@@ -11,30 +11,32 @@ export default Vue.extend({
     }
   },
   render(h: CreateElement): VNode {
-    const children = this.$slots.default?.map((child: VNode, index: number) => {
-      const options = child.componentOptions
-      if (options && options.tag === 'z-tab') {
-        const props = {
-          index,
-          updateActiveIndex: () => {
-            const $parent: any = this.$parent
-            $parent.updateActiveIndex(index)
-          },
-          ...options.propsData
+    const children = this.$slots.default?.map(
+      (child: VNode, index: number): VNode => {
+        const options = child.componentOptions
+        if (options && options.tag === 'z-tab') {
+          const props = {
+            index,
+            updateActiveIndex: () => {
+              const $parent: any = this.$parent
+              $parent.updateActiveIndex(index)
+            },
+            ...options.propsData
+          }
+          return h(
+            ZTab,
+            {
+              ...child.data,
+              props: {
+                ...props
+              }
+            },
+            options.children
+          )
         }
-        return h(
-          ZTab,
-          {
-            ...child.data,
-            props: {
-              ...props
-            }
-          },
-          options.children
-        )
+        return child
       }
-      return child
-    })
+    )
 
     return h('div', { class: 'z-tab-list -mx-3 overflow-auto flex flex-nowrap' }, children)
   }
