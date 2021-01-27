39b0acddac4e3f7c1f77a546febec4c2d270336b
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2021-01-27 10:21:29 +0530

    Add tag implementation with stories and tests

---
+++ src/components/ZTag/ZTag.stories.ts
@@ -0,0 +1,88 @@
+import '../../assets/css/tailwind.css'
+import '../../assets/css/typography.css'
+import '../../assets/css/layout.css'
+import ZTag from './ZTag.vue'
+
+export default {
+  title: 'Tags',
+  component: ZTag,
+  excludeStories: /.*Data$/
+}
+
+export const DefaultTag = () => ({
+  components: { ZTag },
+  template: `<div class='padded-container'>
+        <z-tag>Tag</z-tag>
+    </div>`
+})
+
+export const ColoredTag = () => ({
+  components: { ZTag },
+  template: `<div class='padded-container'>
+        <z-tag state="success">Tag</z-tag>
+        <z-tag state="warning">Tag</z-tag>
+        <z-tag state="error">Tag</z-tag>
+        <z-tag state="info">Tag</z-tag>
+    </div>`
+})
+
+export const TagWithLeftIcon = () => ({
+  components: { ZTag },
+  template: `<div class='padded-container'>
+        <z-tag state="success" icon-left="star">Tag</z-tag>
+    </div>`
+})
+
+export const TagWithRightIcon = () => ({
+  components: { ZTag },
+  template: `<div class='padded-container'>
+        <z-tag state="success" icon-right="star">Tag</z-tag>
+    </div>`
+})
+
+export const TagsWithBothIcons = () => ({
+  components: { ZTag },
+  template: `<div class='padded-container'>
+        <z-tag state="success" icon-left="star" icon-right="star">Tag</z-tag>
+    </div>`
+})
+
+export const LargeTags = () => ({
+  components: { ZTag },
+  template: `<div class='padded-container'>
+        <z-tag state="success" size="large">Tag</z-tag>
+    </div>`
+})
+
+export const LargeTagWithLeftIcon = () => ({
+  components: { ZTag },
+  template: `<div class='padded-container'>
+        <z-tag size="large" icon-left="star">Tag</z-tag>
+        <z-tag state="success" size="large" icon-left="star">Tag</z-tag>
+        <z-tag state="warning" size="large" icon-left="star">Tag</z-tag>
+        <z-tag state="error" size="large" icon-left="star">Tag</z-tag>
+        <z-tag state="info" size="large" icon-left="star">Tag</z-tag>
+    </div>`
+})
+
+export const LargeTagWithRightIcon = () => ({
+  components: { ZTag },
+  template: `<div class='padded-container'>
+        <z-tag size="large" icon-right="star">Tag</z-tag>
+        <z-tag state="success" size="large" icon-right="star">Tag</z-tag>
+        <z-tag state="warning" size="large" icon-right="star">Tag</z-tag>
+        <z-tag state="error" size="large" icon-right="star">Tag</z-tag>
+        <z-tag state="info" size="large" icon-right="star">Tag</z-tag>
+    </div>`
+})
+
+export const LargeTagWithBothIcons = () => ({
+  components: { ZTag },
+  template: `<div class='padded-container'>
+        <z-tag size="large" icon-left="star" icon-right="star">Tag</z-tag>
+        <z-tag state="success" size="large" icon-left="star" icon-right="star">Tag</z-tag>
+        <z-tag state="warning" size="large" icon-left="star" icon-right="star">Tag</z-tag>
+        <z-tag state="error" size="large" icon-left="star" icon-right="star">Tag</z-tag>
+        <z-tag state="info" size="large" icon-left="star" icon-right="star">Tag</z-tag>
+    </div>`
+})
---
+++ tests/unit/ZTag.spec.ts
@@ -0,0 +1,108 @@
+import ZTag from '../../src/components/ZTag'
+
+import { shallowMount } from '@vue/test-utils'
+
+const slotData = {
+  default: 'Tag'
+}
+
+describe('Tags', () => {
+    it('renders a default tag', () => {
+        const wrapper = shallowMount(ZTag, {
+        slots: slotData
+        })
+
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('renders with left icon', () => {
+        const wrapper = shallowMount(ZTag, {
+            propsData: {
+            iconLeft: 'star'
+            },
+            slots: slotData
+        })
+
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('renders with right icon', () => {
+        const wrapper = shallowMount(ZTag, {
+        propsData: {
+            iconRight: 'star'
+        },
+        slots: slotData
+        })
+
+        expect(wrapper.html()).toMatchSnapshot()
+    })    
+
+    it('renders with state', () => {
+        expect(
+            shallowMount(ZTag, {
+                propsData: {
+                    state: 'success'
+                },
+                slots: slotData
+            }).html()
+        ).toMatchSnapshot()
+
+        expect(
+            shallowMount(ZTag, {
+                propsData: {
+                    state: 'error'
+                },
+                slots: slotData
+            }).html()
+        ).toMatchSnapshot()
+
+        expect(
+            shallowMount(ZTag, {
+                propsData: {
+                state: 'warning'
+                },
+                slots: slotData
+            }).html()
+        ).toMatchSnapshot()
+
+        expect(
+            shallowMount(ZTag, {
+                propsData: {
+                state: 'info'
+                },
+                slots: slotData
+            }).html()
+        ).toMatchSnapshot()
+
+        expect(
+            shallowMount(ZTag, {
+                propsData: {
+                state: 'default'
+                },
+                slots: slotData
+            }).html()
+        ).toMatchSnapshot()
+    })
+
+  it('renders with size', () => {
+    expect(
+      shallowMount(ZTag, {
+          propsData: {
+            iconLeft: 'star',
+            size: 'small'
+        },
+        slots: slotData
+      }).html()
+    ).toMatchSnapshot()
+
+    expect(
+      shallowMount(ZTag, {
+        propsData: {
+            iconLeft: 'star',
+            size: 'large'
+        },
+        slots: slotData
+      }).html()
+    ).toMatchSnapshot()
+  })
+})
