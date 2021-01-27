07d2eb30008bcfec5e3d071455d1467a6bc87860
Shivam Mishra <shivam@deepsource.io>
2021-01-27 16:31:59 +0530

    chore: update import and exports

--- src/components/ZMenu/index.ts
+++ src/components/ZMenu/index.ts
@@ -1,3 +1,7 @@
 import ZMenu from './ZMenu.vue'
+import ZMenuItem from './ZMenuItem.vue'
+import ZMenuSection from './ZMenuSection.vue'
+
+export { ZMenu, ZMenuSection, ZMenuItem }
 
 export default ZMenu
