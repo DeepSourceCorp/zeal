828817520c716bec526276627747dd40e2f5635c
Shivam Mishra <shivam@deepsource.io>
2021-01-28 23:43:31 +0530

    feat: add ZNav component

---
+++ src/components/ZNavItem/ZNavItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <button
+    role="tab"
+    :aria-selected="selected"
+    @click="clickHandler"
+    class="inline-flex items-end gap-2 pb-3 px-1 text-sm leading-none outline-none z-nav-item focus:outline-none border-b-2"
+    :disabled="disabled"
+    :class="{
+      'text-vanilla-100 border-juniper': isActive && !disabled,
+      'text-vanilla-400 border-transparent': !isActive,
+      'cursor-auto border-transparent': disabled
+    }"
+  >
+    <z-icon
+      v-if="icon"
+      :icon="icon"
+      size="small"
+      :color="isActive && !disabled ? 'vanilla-100' : 'vanilla-400'"
+    ></z-icon>
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ZIcon from '../ZIcon/ZIcon.vue'
+
+export default Vue.extend({
+  name: 'ZNavItem',
+  components: {
+    ZIcon
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    isActive: {
+      type: Boolean,
+      default: false
+    },
+    icon: {
+      type: String
+    }
+  },
+  computed: {
+    selected(): string {
+      return String(this.isActive)
+    }
+  }
+})
+</script>
