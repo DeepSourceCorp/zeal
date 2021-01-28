156d7e854423fceee18972a9f9373fe119904685
Aman Sharma <aman@deepsource.io>
2021-01-28 11:26:23 +0530

    WIP: Hero
    Signed-off-by: Aman Sharma <aman@deepsource.io>

---
+++ src/components/marketing/sections/ZHero/ZHero.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="w-full">
+    <img class="mx-auto w-80 h-56" src="https://i.imgur.com/flZRSEm.png">
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ZHero',
+  props: {},
+  data() {
+    return {}
+  },
+  methods: {}
+})
+</script>
