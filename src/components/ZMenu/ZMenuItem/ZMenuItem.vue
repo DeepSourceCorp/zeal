585b141f3cf1e77575a2f9debf9feed0f6234eda
Shivam Mishra <shivam@deepsource.io>
2021-01-29 14:23:54 +0530

    feat: update zmenu styles

--- src/components/ZMenu/ZMenuItem/ZMenuItem.vue
+++ src/components/ZMenu/ZMenuItem/ZMenuItem.vue
@@ -2,7 +2,7 @@
   <div
     class="text-sm z-menu-item flex items-center gap-2 px-3 py-2 leading-none outline-none focus:outline-none"
     :class="{
-      'hover:bg-ink-100 cursor-pointer': action && !disabled,
+      'hover:bg-ink-100 cursor-pointer': !disabled,
       'text-vanilla-400 cursor-not-allowed': disabled
     }"
     v-on:click="action()"
