f282e0a0665e64bdc1480e7ddb1fbdabdb177890
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2021-01-29 08:50:19 +0530

    Add Sidebar section and component

---
+++ src/components/ZSidebarMenu/ZSidebarItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sidebar-item flex space-x-2 px-2 py-2 items-center cursor-pointer hover:bg-ink-300 text-sm">
+    <z-icon v-if="icon" 
+            :icon="icon" 
+            size="small"
+            :color="iconColor"></z-icon>
+    <div class="items-center space-x-1 w-full hidden sm:flex"><slot></slot></div>
+  </div>
+</template>
+
+<script>
+import ZIcon from '@/components/ZIcon/ZIcon.vue'
+export default {
+  name: 'ZSidebarMenuItem',
+  components: {
+    ZIcon
+  },
+  props: {
+    icon: {
+      type: String,
+    },
+    iconColor: {
+      type: String
+    }
+  }
+}
+</script>
