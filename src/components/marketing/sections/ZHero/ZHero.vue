b9d0a7673c861e3cdb350a03b2ffe18542317285
Aman Sharma <aman@deepsource.io>
2021-02-01 16:20:52 +0530

    fix: padding and font sizes.
    Signed-off-by: Aman Sharma <aman@deepsource.io>

--- src/components/marketing/sections/ZHero/ZHero.vue
+++ src/components/marketing/sections/ZHero/ZHero.vue
@@ -2,12 +2,12 @@
   <section class="z-hero__section w-full relative text-vanilla-100 text-center">
     <span class="z-hero__content block">
       <h1
-        class="block text-vanilla-100 mb-6 text-3xl lg:text-5xl leading-11 lg:leading-12 w-auto lg:w-4/5 font-bold mx-auto sm:px-10 md:px-5"
+        class="block text-vanilla-100 mb-3 sm:mb-6 text-2xl sm:text-3xl lg:text-5xl leading-10 sm:leading-11 lg:leading-12 w-auto lg:w-4/5 font-bold mx-auto sm:px-10 md:px-5"
       >
         <slot name="heading"></slot>
       </h1>
       <h2
-        class="block text-lg lg:text-lg leading-9 lg:leading-normal w-auto lg:w-1/2 font-normal text-vanilla-200 mb-12 mx-auto px-5 sm:px-32 md:px-16"
+        class="block text-sm sm:text-lg lg:text-lg leading-6 sm:leading-9 lg:leading-normal w-auto lg:w-1/2 font-normal text-vanilla-200 mb-12 mx-auto px-5 sm:px-32 md:px-16"
       >
         <slot name="subheading"></slot>
       </h2>
@@ -16,7 +16,7 @@
       </span>
     </span>
 
-    <div class="z-hero-illustration__content mt-10 lg:mt-0">
+    <div class="z-hero-illustration__content mt-10 lg:-mt-12">
       <!-- This div is for scaling from a (a < 1) to b (a < b <= 1) -->
       <div class="z-hero-illustration__container">
         <!-- This div for rotating on the z-axis from `a`deg (a > 0deg) to `b`deg ( 0deg <= b < a) -->
@@ -73,11 +73,11 @@ export default Vue.extend({
         translateZ(0px)`
     },
     getScaleValue(scrollY: number): number {
-      const calc = MIN_SCALE + (scrollY * 0.06) / 100
+      const calc = MIN_SCALE + (scrollY * 0.08) / 100
       return calc < MAX_SCALE ? calc : MAX_SCALE
     },
     getRotationValue(scrollY: number): number {
-      const calc = MAX_ROTATION_ANGLE - (scrollY * 0.5) / 10
+      const calc = MAX_ROTATION_ANGLE - (scrollY * 0.6) / 10
       return calc > 0 ? calc : MIN_ROTATION_ANGLE
     }
   }
