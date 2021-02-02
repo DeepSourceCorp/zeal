1b2c95b69d81d3f8b54af451cd6e8b47562a64b9
Shivam Mishra <shivam@deepsource.io>
2021-02-02 12:04:22 +0530

    fix: pager for 3 items

--- src/components/ZPagination/ZPagination.stories.ts
+++ src/components/ZPagination/ZPagination.stories.ts
@@ -13,9 +13,17 @@ export default {
 export const BasicMenu = () => ({
   components: { ZPagination },
   template: `
-    <div class="container">
+    <div class="container space-y-5">
       <z-pagination
         :totalPages="12"
+        :totalVisible="5"
+      ></z-pagination>
+      <z-pagination
+        :totalPages="3"
+        :totalVisible="5"
+      ></z-pagination>
+      <z-pagination
+        :totalPages="25"
         :totalVisible="3"
       ></z-pagination>
     </div>`
