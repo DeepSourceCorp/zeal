dc04e6649e37d70c4d30c4c9b0dc1edc83d83f35
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2021-02-02 13:29:48 +0530

    Export Radio button

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ import ZExpandable from './components/ZExpandable/index'
 import ZInput from './components/ZInput/index'
 import ZRadio from './components/ZRadio/index'
 import ZRadioGroup from './components/ZRadioGroup/index'
+import ZRadioButton from './components/ZRadioButton/index'
 import ZSelect from './components/ZSelect/index'
 import ZOption from './components/ZOption/index'
 import ZAvatar from './components/ZAvatar/index'
@@ -51,6 +52,7 @@ const components = [
   ZExpandable,
   ZRadio,
   ZRadioGroup,
+  ZRadioButton,
   ZInput,
   ZSelect,
   ZOption,
@@ -94,6 +96,7 @@ export default {
   ZExpandable,
   ZRadio,
   ZRadioGroup,
+  ZRadioButton,
   ZInput,
   ZSelect,
   ZOption,
@@ -127,6 +130,7 @@ export { default as ZExpandable } from './components/ZExpandable/index'
 export { default as ZInput } from './components/ZInput/index'
 export { default as ZRadio } from './components/ZRadio/index'
 export { default as ZRadioGroup } from './components/ZRadioGroup/index'
+export { default as ZRadioButton } from './components/ZRadioButton/index'
 export { default as ZSelect } from './components/ZSelect/index'
 export { default as ZOption } from './components/ZOption/index'
 export { default as ZLabel } from './components/ZLabel/index'
