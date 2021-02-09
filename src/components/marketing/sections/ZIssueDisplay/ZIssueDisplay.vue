a8bda0bd42ced8017bd3e54953d4d2d4b4ec6506
Aman Sharma <aman@deepsource.io>
2021-02-09 13:28:29 +0530

    fix: WIP
    Signed-off-by: Aman Sharma <aman@deepsource.io>

--- src/components/marketing/sections/ZIssueDisplay/ZIssueDisplay.vue
+++ src/components/marketing/sections/ZIssueDisplay/ZIssueDisplay.vue
@@ -1,34 +1,51 @@
 <template>
   <section class="text-vanilla-100">
-    <div class="w-1/2 ml-auto sticky top-20">
-      <div class="flex">
+    <div class="w-1/2 h-full ml-auto sticky top-20">
+      <!-- Analyzers section -->
+      <div class="flex mb-12">
         <div>
-          <img :src="activeAnalyzer.image" :alt="activeAnalyzer.name" class="h-14" />
-          <div class="flex items-center text-3xl">
+          <img :src="activeAnalyzer.image" :alt="activeAnalyzer.name" class="h-14 mb-2" />
+          <div class="flex items-center leading-11 text-3xl">
             {{ activeAnalyzer.label }}
-            <z-icon size="medium" icon="chevron-left"></z-icon>
-            <z-icon size="medium" icon="chevron-right"></z-icon>
+            <span class="flex mt-2 ml-3 mr-6">
+              <z-icon size="medium" icon="chevron-left"></z-icon>
+              <z-icon size="medium" icon="chevron-right"></z-icon>
+            </span>
           </div>
         </div>
         <div class="grid grid-cols-4">
-          <div v-for="analyzer in analyzers" :key="analyzer.name" class="flex items-center mr-2">
-            <img :src="analyzer.image" :alt="analyzer.name" class="mr-2 h-5" />
+          <div
+            v-for="analyzer in analyzers"
+            :key="analyzer.name"
+            class="flex items-center mr-2 text-sm"
+          >
+            <img :src="analyzer.image" :alt="analyzer.name" class="mx-2 h-5" />
             {{ analyzer.label }}
           </div>
         </div>
       </div>
+
+      <!-- Issues section -->
+      <div class="bg-gradient-ocean pl-2 pt-2 h-full">
+        <div class="bg-ink-400">
+          asdf
+        </div>
+      </div>
     </div>
-    <div>
-      <div class="sticky top-3/4">Sticky Heading 1</div>
-      <div class="pb-102"></div>
-    </div>
-    <div>
-      <div class="sticky top-3/4">Sticky Heading 2</div>
-      <div class="pb-102"></div>
-    </div>
-    <div>
-      <div class="sticky top-3/4">Sticky Heading 3</div>
-      <div class="pb-102"></div>
+
+    <div class="w-1/2">
+      <div>
+        <div class="sticky top-3/4">Sticky Heading 1</div>
+        <div class="pb-102"></div>
+      </div>
+      <div>
+        <div class="sticky top-3/4">Sticky Heading 2</div>
+        <div class="pb-102"></div>
+      </div>
+      <div>
+        <div class="sticky top-3/4">Sticky Heading 3</div>
+        <div class="pb-102"></div>
+      </div>
     </div>
   </section>
 </template>
