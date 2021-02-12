80de1f8506319417f3dec2d1bdd5adc85b2c9c85
Ruphaa Ganesh <ruphaa@deepsource.io>
2021-02-12 21:06:18 +0530

    Update the screen size
    Signed-off-by: Ruphaa Ganesh <ruphaa@deepsource.io>

--- src/components/ZAccordion/ZAccordion.stories.ts
+++ src/components/ZAccordion/ZAccordion.stories.ts
@@ -181,19 +181,19 @@ export const WebNextFooter = () => ({
   },
   computed:  {
     isMobile() {
-      return window.innerWidth < 640;
+      return window.innerWidth < 768;
     }
   },
   template: `<div class="h-screen w-full-screen">
         <div class="prose prose-md max-w-none p-20">
         <h1 v-for="i in 8">Some random heading to grab attention</h1>
         </div>
-        <footer class="w-full p-12 flex flex-col sm:space-y-0 space-y-2 sm:grid grid-rows-footer grid-cols-footer gap-x-8 gap-y-4 bg-transparent text-vanilla-100 border-t border-ink-200 min-h-102 bg-gradient-dark_dawn animate-gradient bg-400%">
+        <footer class="w-full p-12 flex flex-col md:space-y-0 space-y-2 md:grid grid-rows-footer grid-cols-footer gap-x-8 gap-y-4 bg-transparent text-vanilla-100 border-t border-ink-200 min-h-102 bg-gradient-dark_dawn animate-gradient bg-400%">
             <div class="flex row-start-1 row-end-1 col-start-1 col-end-1">
                 <img class="max-w-none h-8" src="https://i.imgur.com/zKLLWIr.png"/>
             </div>
 
-            <z-accordion-item :is-list="!isMobile" title="Product" class="row-start-1 row-end-1 col-start-2 col-end-2 flex flex-col px-0 py-0 space-y-2 sm:space-y-6 text-vanilla-400 tracking-wide uppercase">
+            <z-accordion-item :is-list="!isMobile" title="Product" class="row-start-1 row-end-1 col-start-2 col-end-2 flex flex-col px-0 py-0 space-y-2 md:space-y-6 text-vanilla-400 tracking-wide uppercase">
               <div class="flex flex-col space-y-2">
                 <a v-for="item in product" :key="item" href="#" class="hover:text-juniper transition-all duration-75 ease-in-out flex flex-col space-y-4 text-vanilla-300 capitalize">
                     {{item}}
@@ -201,7 +201,7 @@ export const WebNextFooter = () => ({
               </div>
             </z-accordion-item>
 
-            <z-accordion-item :is-list="!isMobile" title="languages" class="row-start-1 row-end-1 col-start-3 col-end-3 flex flex-col px-0 py-0  space-y-2 sm:space-y-6 text-vanilla-400 tracking-wide uppercase">
+            <z-accordion-item :is-list="!isMobile" title="languages" class="row-start-1 row-end-1 col-start-3 col-end-3 flex flex-col px-0 py-0  space-y-2 md:space-y-6 text-vanilla-400 tracking-wide uppercase">
               <div class="flex flex-col space-y-2">
                 <a v-for="language in languages" :key="language" href="#" class="hover:text-juniper transition-all duration-75 ease-in-out flex flex-col space-y-4 text-vanilla-300 capitalize">
                     {{language}}
@@ -209,7 +209,7 @@ export const WebNextFooter = () => ({
               </div>
             </z-accordion-item>
 
-            <z-accordion-item :is-list="!isMobile" title="resources" class="row-start-1 row-end-1 col-start-4 col-end-4 flex flex-col px-0 py-0  space-y-2 sm:space-y-6 text-vanilla-400 tracking-wide uppercase">
+            <z-accordion-item :is-list="!isMobile" title="resources" class="row-start-1 row-end-1 col-start-4 col-end-4 flex flex-col px-0 py-0  space-y-2 md:space-y-6 text-vanilla-400 tracking-wide uppercase">
               <div class="flex flex-col space-y-2">
                 <a v-for="item in resources" :key="item" href="#" class="hover:text-juniper transition-all duration-75 ease-in-out flex flex-col space-y-4 text-vanilla-300 capitalize">
                     {{item}}
@@ -217,7 +217,7 @@ export const WebNextFooter = () => ({
               </div>
             </z-accordion-item>
 
-            <z-accordion-item :is-list="!isMobile" title="company" class="row-start-1 row-end-1 col-start-5 col-end-5 flex flex-col px-0 py-0  space-y-2 sm:space-y-6 text-vanilla-400 tracking-wide uppercase">
+            <z-accordion-item :is-list="!isMobile" title="company" class="row-start-1 row-end-1 col-start-5 col-end-5 flex flex-col px-0 py-0  space-y-2 md:space-y-6 text-vanilla-400 tracking-wide uppercase">
               <div class="flex flex-col space-y-2">
                 <a v-for="item in company" :key="item" href="#" class="hover:text-juniper transition-all duration-75 ease-in-out flex flex-col space-y-4 text-vanilla-300 capitalize">
                     {{item}}
@@ -226,10 +226,10 @@ export const WebNextFooter = () => ({
             </z-accordion-item>
 
             <div class="row-start-1 row-end-1 col-start-6 col-end-6 flex flex-col space-y-6 text-vanilla-400 text-sm">
-                <div class="font-medium uppercase tracking-wide hidden sm:block">Social Media</div>
-                <div class="flex flex-col-reverse sm:flex-col gap-y-3 sm:gap-y-16">
-                  <div class="flex flex-col space-y-4 items-center sm:items-start">
-                    <div class="text-vanilla-300 hidden sm:block">Follow us on social media to stay updated.</div>
+                <div class="font-medium uppercase tracking-wide hidden md:block">Social Media</div>
+                <div class="flex flex-col-reverse md:flex-col gap-y-3 md:gap-y-16">
+                  <div class="flex flex-col space-y-4 items-center md:items-start">
+                    <div class="text-vanilla-300 hidden md:block">Follow us on social media to stay updated.</div>
                     <div class="flex space-x-3">
                         <z-icon size="medium" icon="dribbble" color="vanilla-300"></z-icon>
                         <z-icon size="medium" icon="dribbble" color="vanilla-300"></z-icon>
@@ -247,11 +247,11 @@ export const WebNextFooter = () => ({
                 </div>
             </div>
 
-            <div class="block sm:hidden w-full h-px bg-slate"></div>
+            <div class="block md:hidden w-full h-px bg-slate"></div>
 
             <div class="row-start-1 row-end-1 col-start-7 col-end-7 text-sm text-vanilla-400">
-                <div class="flex justify-center flex-row space-x-2 sm:space-x-0 sm:flex-col space-y-0 sm:space-y-2">
-                    <div v-for="i in 2" :key="i" class="flex flex-col sm:grid grid-rows-3 grid-cols-3 gap-x-4 p-2 sm:p-2.5 tracking-wide bg-ink-300 rounded-sm w-auto sm:w-full h-auto sm:h-20 flex items-center">
+                <div class="flex justify-center flex-row space-x-2 md:space-x-0 md:flex-col space-y-0 md:space-y-2">
+                    <div v-for="i in 2" :key="i" class="flex flex-col md:grid grid-rows-3 grid-cols-3 gap-x-4 p-2 md:p-2.5 tracking-wide bg-ink-300 rounded-sm w-auto md:w-full h-auto md:h-20 flex items-center">
                         <img class="max-w-none row-span-3" src="https://i.imgur.com/JBKMaq5.png"/>
                         <span class="uppercase font-medium col-span-2 text-vanilla-100">GDPR</span>
                         <span class="row-span-2 col-span-2 tracking-normal">compliant</span>
@@ -259,7 +259,7 @@ export const WebNextFooter = () => ({
                 </div>
             </div>
 
-            <span class="order-last self-center row-start-2 row-end-2 col-start-1 col-end-4 self-end text-left text-vanilla-400 text-xs sm:text-sm">
+            <span class="order-last self-center row-start-2 row-end-2 col-start-1 col-end-4 self-end text-left text-vanilla-400 text-xs md:text-sm">
                 © 2020, DeepSource Corp. All rights reserved.
             </span>
 
