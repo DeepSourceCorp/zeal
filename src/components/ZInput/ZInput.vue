83a09fa718056747fc72a1057f8d559bb4e532be
Ruphaa Ganesh <ruphaa@deepsource.io>
2021-02-12 13:22:12 +0530

    Add Input addon and input group
    Signed-off-by: Ruphaa Ganesh <ruphaa@deepsource.io>

--- src/components/ZInput/ZInput.vue
+++ src/components/ZInput/ZInput.vue
@@ -1,46 +1,46 @@
 <template>
-  <div class="z-input w-full relative">
-    <span
-      v-if="icon && iconPosition === 'left'"
-      class="absolute h-full flex items-center top-0 text-center transition-all left-2 cursor-pointer"
-    >
-      <z-icon :icon="icon" size="small"></z-icon>
-    </span>
+  <div
+    class="flex items-center w-full rounded-sm outline-none space-x-2 p-1 focus-within:shadow-white"
+    :class="[
+      borderStyles,
+      `text-${textSize}`,
+      `bg-${backgroundColor}`,
+      {
+        'text-slate cursor-not-allowed': disabled,
+        'text-vanilla-300': !disabled
+      }
+    ]"
+  >
+    <!-- Any icon/content to the left renders here -->
+    <slot name="left"></slot>
     <input
       type="text"
-      class="input w-full caret-juniper focus:shadow-white rounded-sm outline-none"
-      :class="[
-        `${(disabled && 'text-slate cursor-not-allowed') || 'text-vanilla-300'}`,
-        getIconBasedStyle,
-        borderStyles,
-        `text-${textSize}`,
-        `bg-${backgroundColor}`
-      ]"
+      class="w-full caret-juniper flex flex-grow outline-none bg-transparent"
+      :class="{
+        'cursor-not-allowed': disabled
+      }"
       :value="name"
       :placeholder="placeholder"
       :disabled="disabled"
       @input="updateSelf($event.target.value)"
     />
-    <span
-      v-if="clearable && !icon"
-      class="absolute h-full flex items-center top-0 text-center right-2 transition-all cursor-pointer"
-      @click.stop="updateSelf('')"
-    >
-      <z-icon icon="x" size="small"></z-icon>
-    </span>
-    <span
-      v-if="icon && iconPosition === 'right'"
-      class="absolute h-full flex items-center top-0 text-center transition-all right-2 cursor-pointer"
-    >
-      <z-icon :icon="icon" size="small"></z-icon>
-    </span>
+    <!-- Any icon/content to the right renders here -->
+    <slot name="right">
+      <z-icon
+        v-if="clearable"
+        icon="x"
+        size="small"
+        class="cursor-pointer"
+        @click.stop="updateSelf('')"
+      >
+      </z-icon>
+    </slot>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import ZIcon from '../ZIcon/ZIcon.vue'
-
 export default Vue.extend({
   name: 'ZInput',
   components: {
@@ -65,20 +65,9 @@ export default Vue.extend({
     clearable: {
       type: Boolean
     },
-    iconPosition: {
-      type: String,
-      default: 'left'
-    },
     textSize: {
       type: String,
       default: 'sm'
-    },
-    spacing: {
-      type: String,
-      default: 'base',
-      validator(value): boolean {
-        return ['tight', 'base', 'loose'].includes(value)
-      }
     },
     backgroundColor: {
       type: String,
@@ -94,22 +83,6 @@ export default Vue.extend({
     event: 'input'
   },
   computed: {
-    getIconBasedStyle(): string {
-      const iconPositions = {
-        left: `pl-8 ${this.getSpacingClass}`,
-        right: `pr-8 ${this.getSpacingClass}`,
-        none: `${this.getSpacingClass}`
-      }
-      return (this.icon && iconPositions[this.iconPosition]) || iconPositions['none']
-    },
-    getSpacingClass(): string {
-      const spacers: Record<string, string> = {
-        tight: 'px-3 py-1.5',
-        base: 'p-3',
-        loose: 'px-3 py-4'
-      }
-      return spacers[this.spacing || 'base']
-    },
     borderStyles(): string {
       if (this.showBorder) {
         return 'focus:border-vanilla-400 border border-solid border-ink-100'
---
+++ src/components/ZInputAddon/ZInputAddon.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center outline-none relative">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'ZInputAddon'
+})
+</script>
