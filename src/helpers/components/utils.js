1aac8f44d56f8089ee484a4342810b47f48cb749
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2021-02-15 15:02:58 +0530

    Add List and List Item Component with its stories (#105)

---
+++ src/helpers/components/utils.js
@@ -0,0 +1,12 @@
+export const toPascal = (word) => {
+  // https://stackoverflow.com/a/53952925
+  return `${word}`
+    .replace(new RegExp(/[-_]+/, 'g'), ' ')
+    .replace(new RegExp(/[^\w\s]/, 'g'), '')
+    .replace(
+      new RegExp(/\s+(.)(\w+)/, 'g'),
+      ($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`
+    )
+    .replace(new RegExp(/\s/, 'g'), '')
+    .replace(new RegExp(/\w/), (s) => s.toUpperCase())
+}
