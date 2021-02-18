b470de2febeb7b5f90caa4e8ede2f22097b852eb
Ruphaa Ganesh <ruphaa@deepsource.io>
2021-02-18 21:39:38 +0530

    Add Code component files
    Signed-off-by: Ruphaa Ganesh <ruphaa@deepsource.io>

---
+++ src/components/ZCode/ZCode.stories.ts
@@ -0,0 +1,24 @@
+import '../../assets/css/tailwind.css'
+import '../../assets/css/typography.css'
+import '../../assets/css/layout.css'
+import ZCode from './ZCode.vue'
+
+export default {
+  title: 'Code',
+  component: ZCode,
+  excludeStories: /.*Data$/
+}
+
+export const DefaultCode = () => ({
+    components: { ZCode },
+    data() { 
+        return { 
+            content: `<div class='input-container'>
+        <z-code :content="content"></z-code>           
+    </div>`
+        }
+    },
+    template: `<div class='input-container'>
+        <z-code :content="content"></z-code>           
+    </div>`
+})
---
+++ src/components/ZCode/ZCode.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="z-code" v-html="content">
+  </div>
+</template>
+<script>
+export default {
+  name: 'ZCode',
+  props: {
+    content: {
+      type: String
+    }
+  }
+}
+</script>
+<style>
+@import '../../assets/css/syntax_highlighter.css';
+</style>
