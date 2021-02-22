218e5ac56418be1e284d39f6c78e2627cdff926e
Ruphaa Ganesh <ruphaa@deepsource.io>
2021-02-22 14:11:58 +0530

    Add test files
    Signed-off-by: Ruphaa Ganesh <ruphaa@deepsource.io>

--- src/components/ZCode/ZCode.stories.ts
+++ src/components/ZCode/ZCode.stories.ts
@@ -12,7 +12,7 @@ export default {
 export const DefaultCode = () => ({
     components: { ZCode },
     data() { 
-        return { 
+        return {
             content: `<div class="highlight"><pre><span class="ln">2</span>\n<span class="ln">3</span>\n<span class="ln">4</span><span class="nd">@dramatiq.actor</span>\n<span class="hl"><span class="ln">5</span><span class="k">def</span> <span class="nf">example</span><span class="p">(</span><span class="p">)</span><span class="p">:</span></span><span class="ln">6</span>    <span class="k">pass</span>\n</pre></div>`
         }
     },
