b1b8abca81bf663bbb62c02a796063606bc6ff4a
Ruphaa Ganesh <ruphaa@deepsource.io>
2021-03-05 12:14:11 +0530

    Add files for table component
    Signed-off-by: Ruphaa Ganesh <ruphaa@deepsource.io>

---
+++ src/components/ZTable/ZTable.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="z-table"></div>
+</template>
+
+<script>
+export default {
+  name: 'ZTable',
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    },
+    disabled: {
+      type: Boolean
+    }
+  },
+  computed: {},
+  methods: {}
+}
+</script>
