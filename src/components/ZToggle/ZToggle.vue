8925b502b5cdcbbb90eeeea8715cf8b8c908aa63
ruphaa-deepsource <73866406+ruphaa-deepsource@users.noreply.github.com>
2021-03-08 11:29:28 +0530

    [PLT-1292]: Add Toggle Component along with the stories and tests (#124)
    * Add Toggle implementation along with the stories and tests
    
    Signed-off-by: Ruphaa Ganesh <ruphaa@deepsource.io>
    
    * Address review comments and VQA
    
    Signed-off-by: Ruphaa Ganesh <ruphaa@deepsource.io>
    
    * Format code with prettier
    
    * Remove usused styles
    
    Signed-off-by: Ruphaa Ganesh <ruphaa@deepsource.io>
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ src/components/ZToggle/ZToggle.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="inline-block cursor-pointer w-8 h-5 rounded-full focus:outline-none"
+    role="checkbox"
+    :aria-checked="value.toString()"
+    tabindex="0"
+    @click="toggle(!value)"
+    @keydown.space.prevent="toggle"
+    :class="{
+      'cursor-not-allowed': this.disabled
+    }"
+  >
+    <span
+      class="inline-block rounded-full h-full w-full transition-DEFAULT duration-300 ease-in-out p-0.5 flex items-center"
+      :class="[backgroundStyles]"
+    >
+      <span
+        class="inline-block w-3.5 h-3.5 transform rounded-full transition-transform ease-in-out duration-300 shadow-grey"
+        :class="{
+          'bg-vanilla-100': !this.disabled,
+          'bg-vanilla-400 opacity-50': this.disabled,
+          'translate-x-full': this.value,
+          'translate-x-0': !this.value
+        }"
+      />
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ZToggle',
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    },
+    disabled: {
+      type: Boolean
+    }
+  },
+  model: {
+    prop: 'value',
+    event: 'input'
+  },
+  computed: {
+    backgroundStyles() {
+      if (this.disabled) {
+        return {
+          'bg-ink-100': this.value,
+          'bg-slate': !this.value
+        }
+      }
+      return {
+        'bg-juniper': this.value,
+        'bg-vanilla-200': !this.value
+      }
+    }
+  },
+  methods: {
+    toggle(newValue) {
+      if (!this.disabled) this.$emit('input', newValue)
+    }
+  }
+}
+</script>
