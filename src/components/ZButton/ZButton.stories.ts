bfa73e92a8cddb8a74ea141aef5109648cf7c3cc
Shivam Mishra <shivam@deepsource.io>
2021-03-24 18:49:25 +0530

    refactor: z button

--- src/components/ZButton/ZButton.stories.ts
+++ src/components/ZButton/ZButton.stories.ts
@@ -10,102 +10,113 @@ export default {
   excludeStories: /.*Data$/
 }
 
-export const DefaultWithoutText = () => ({
+export const BasicButton = () => ({
   components: { ZButton },
-  template: '<z-button color="primary"></z-button>'
+  template: '<z-button buttonType="primary">Hello World</z-button>'
 })
 
-export const ButtonWithText = () => ({
+export const ButtonWithClickAction = () => ({
   components: { ZButton },
-  template: '<z-button color="primary">Hello World</z-button>'
+  methods: {
+    clicked() {
+      alert('CLICKED BUTTON')
+    }
+  },
+  template: `<z-button buttonType="primary" @click="clicked">Hello World</z-button>`
 })
 
 export const Colors = () => ({
   components: { ZButton },
   template: `<div>
-        <z-button color="primary">Hello World</z-button>
-        <z-button color="secondary">Hello World</z-button>
+        <z-button buttonType="primary">Primary Button</z-button>
+        <z-button buttonType="secondary">Secondary Button</z-button>
+        <z-button buttonType="danger">Danger Button</z-button>
+        <z-button buttonType="ghost">Ghost Button</z-button>
+        <z-button buttonType="link">Link Button</z-button>
     </div>`
 })
 
 export const Sizes = () => ({
   components: { ZButton },
-  template: `<div>
-        <z-button color="primary" size="small">Small Button</z-button>
-        <z-button color="primary" size="medium">Medium Button</z-button>
-        <z-button color="primary" size="large">Large Button</z-button>
-        <z-button color="primary" size="xlarge">Extra Large Button</z-button>
+  filters: {
+    capitalize: function(value: string) {
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  },
+  data() {
+    return {
+      sizes: ['small', 'medium', 'large', 'xlarge'],
+      types: ['primary', 'secondary', 'danger', 'ghost', 'link']
+    }
+  },
+  template: `<div class="space-y-4">
+        <div v-for="size in sizes" :key="size" class="flex space-x-2">
+          <z-button v-for="type in types" :key="type" :buttonType="type" :size="size">{{ size | capitalize }} {{ type | capitalize }}</z-button>
+        </div>
     </div>`
 })
 
 export const States = () => ({
   components: { ZButton },
-  template: `<div>
-        <z-button color="primary" active>Active Button</z-button>
-        <z-button color="primary" disabled>Disabled Button</z-button>
-        <z-button color="secondary" active>Active Secondary Button</z-button>
-        <z-button color="secondary" disabled>Disabled Secondary Button</z-button>
+  filters: {
+    capitalize: function(value: string) {
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  },
+  data() {
+    return {
+      types: ['primary', 'secondary', 'danger', 'ghost', 'link']
+    }
+  },
+  template: `<div class="space-y-4">
+        <div v-for="type in types" :key="type" class="flex space-x-2">
+          <z-button :buttonType="type">Active {{ type | capitalize }}</z-button>
+          <z-button :buttonType="type" disabled>Disabled {{ type | capitalize }}</z-button>
+        </div>
     </div>`
 })
 
 export const IconButton = () => ({
   components: { ZButton },
-  template: `<div>
-        <z-button color="primary"
-                  icon="plus"
-                  iconColor="ink-400"></z-button>
+  data() {
+    return {
+      sizes: ['small', 'medium', 'large', 'xlarge'],
+      types: ['primary', 'secondary', 'danger', 'ghost', 'link']
+    }
+  },
+  template: `<div class="space-y-2">
+        <div v-for="size in sizes" :key="size" class="flex space-x-2">
+          <z-button v-for="type in types" :size="size" :key="type" :buttonType="type" icon="plus" iconColor="ink-400"></z-button>
+        </div>
+    </div>`
+})
+
+export const IconButtonWithText = () => ({
+  components: { ZButton },
+  data() {
+    return {
+      types: ['primary', 'secondary', 'danger', 'ghost', 'link']
+    }
+  },
+  template: `<div class="space-x-2">
+          <z-button v-for="type in types" :size="size" :key="type" :buttonType="type" icon="plus">Button</z-button>
     </div>`
 })
 
 export const SmallIconButton = () => ({
   components: { ZButton },
   template: `<div>
-        <z-button color="primary"
-                  icon="plus"
-                  iconColor="ink-400"
-                  iconSize="small"></z-button>
+        <z-button buttonType="primary" iconColor="ink-400" icon="plus" iconSize="small" size="small"></z-button>
     </div>`
 })
 
 export const FullWidthButton = () => ({
   components: { ZButton },
   template: `<div>
-        <z-button color="primary" full-width>Full Width Button</z-button>
-    </div>`
-})
-
-export const DefaultLinkButton = () => ({
-  components: { ZButton },
-  template: `<div>
-        <z-button color="link" type="link" to="#">Link Button</z-button>
-    </div>`
-})
-
-export const ActiveLinkButton = () => ({
-  components: { ZButton },
-  template: `<div>
-        <z-button color="link" type="link" to="#" active>Link Button</z-button>
-    </div>`
-})
-
-export const DisabledLinkButton = () => ({
-  components: { ZButton },
-  template: `<div>
-        <z-button color="link" type="link" to="#" disabled>Link Button</z-button>
-    </div>`
-})
-
-export const GhostButton = () => ({
-  components: { ZButton },
-  template: `<div class="flex flex-col space-x-2 items-center">
-        <div class="w-full p-6 bg-ink-400">
-          <z-button color="ghost" type="link" to="#" class="text-juniper" hoverOpacity="50">Ghost Link</z-button>
-        </div>
-        <div class="w-full p-6 bg-ink-300">
-          <z-button color="ghost" type="button" class="text-vanilla-100" hoverOpacity="100">Ghost Button</z-button>
-        </div>
-        <div class="w-full p-6">
-          <z-button color="ghost" icon="plus" iconColor="vanilla-100" class="text-vanilla-100"></z-button>
-        </div>
+        <z-button buttonType="primary" full-width>Full Width Button</z-button>
     </div>`
 })
