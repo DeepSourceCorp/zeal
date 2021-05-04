49b26cc13247f9efeb09fc61cbbc8d43064b8395
Shivam Mishra <77610151+shivam-deepsource@users.noreply.github.com>
2021-05-04 22:10:46 +0530

    feat: add 1.5xl font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -244,6 +244,7 @@ module.exports = {
       base: '1rem',
       lg: '1.25rem',
       xl: '1.375rem',
+      '1.5xl': '1.5rem',
       '2xl': '1.75rem',
       '2.5xl': '2rem',
       '3xl': '2.75rem',
