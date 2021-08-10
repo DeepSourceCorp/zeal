85c746c36608d4268730daaa377cb48ac320674f
Rohan <80349145+rohan-deepsource@users.noreply.github.com>
2021-08-10 22:35:22 +0530

    Increase y translation for float animation (#296)

--- tailwind.config.js
+++ tailwind.config.js
@@ -902,7 +902,7 @@ module.exports = {
           transform: 'translate(0,  0px)'
         },
         '50%': {
-          transform: 'translate(0, 15px)'
+          transform: 'translate(0, 30px)'
         },
         '100%': {
           transform: 'translate(0, -0px)'
