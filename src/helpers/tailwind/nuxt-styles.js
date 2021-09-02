e0abe08fb624d87c4f8c815565f2f20e3e5fb088
Rohan <rohan@deepsource.io>
2021-09-02 22:41:48 +0530

    feat: add before element to headings to fix scroll

--- src/helpers/tailwind/nuxt-styles.js
+++ src/helpers/tailwind/nuxt-styles.js
@@ -6,6 +6,14 @@ const contentStyles = function (theme) {
       marginLeft: '-1.5rem',
       color: theme('colors.slate'),
       position: 'absolute'
+    },
+    'h2::before, h3::before, h4::before, h5::before' : {
+      content: '" "',
+      display: 'block',
+      marginTop: '-5rem',
+      height: '5rem',
+      visibility: 'hidden',
+      pointerEvents: 'none'
     }
   }
 }
