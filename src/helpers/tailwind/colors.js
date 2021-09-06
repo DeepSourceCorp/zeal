9f9646de6f01f0dc643d33d9d51e156ea9bee8ff
Shivam Mishra <shivam@deepsource.io>
2021-09-06 15:14:10 +0530

    feat: add new colors

--- src/helpers/tailwind/colors.js
+++ src/helpers/tailwind/colors.js
@@ -2,14 +2,7 @@ module.exports = {
   transparent: 'transparent',
   current: 'currentColor',
   black: '#000000',
-  juniper: '#33cb9a',
-  'light-juniper': '#2EB78B',
-  robin: '#4568dc',
-  slate: '#52575c',
-  cherry: '#da5565',
-  'light-cherry': '#D6435B',
-  honey: '#f6d87c',
-  aqua: '#23c4f8',
+  slate: '#52575C',
   github: '#414141',
   gitlab: '#6753B5',
   bitbucket: '#1E54C5',
@@ -17,11 +10,58 @@ module.exports = {
   lilac: '#C97BD4',
   'sea-glass': '#49F9CF',
   pink: '#F977FF',
+  juniper: {
+    DEFAULT: '#33CB9A',
+    600: '#2EB78B',
+    500: '#33CB9A',
+    400: '#53D4AA',
+    300: '#74DCBA',
+    200: '#94E4CA',
+    150: '#A5E8D2',
+    100: '#B5ECDA'
+  },
+  robin: {
+    DEFAULT: '#4568DC',
+    500: '#',
+    400: '#6784E2',
+    200: '#ACBCEF',
+    300: '#8AA0E9',
+    150: '#BDCAF3',
+    100: '#CED8F6'
+  },
+  cherry: {
+    DEFAULT: '#DA5565',
+    600: '#D6435B',
+    500: '#DA5565',
+    400: '#E17783',
+    300: '#E998A2',
+    200: '#F0BAC0',
+    150: '#F4CACF',
+    100: '#F7DBDE'
+  },
+  honey: {
+    DEFAULT: '#F6D87C',
+    500: '#F6D87C',
+    400: '#F9E3A2',
+    300: '#FBEFC8',
+    200: '#FEFAEF',
+    150: '#FFFFFF',
+    100: '#FFFFFF'
+  },
+  aqua: {
+    DEFAULT: '#23C4F8',
+    500: '#23C4F8',
+    400: '#4BCFF9',
+    300: '#72D9FB',
+    200: '#9AE4FC',
+    150: '#ADE9FC',
+    100: '#C1EEFD'
+  },
   vanilla: {
-    100: '#ffffff',
-    200: '#f5f5f5',
-    300: '#eeeeee',
-    400: '#c0c1c3'
+    100: '#FFFFFF',
+    200: '#F5F5F5',
+    300: '#EEEEEE',
+    400: '#C0C1C3'
   },
   ink: {
     50: '#303540',
