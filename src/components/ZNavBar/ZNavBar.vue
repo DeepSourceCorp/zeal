8b6e07e982979d9481ccdae322c3fcfcea5ed084
Rohan <80349145+rohan-deepsource@users.noreply.github.com>
2021-09-29 20:58:40 +0530

    Fix: ZNavbar mobile menu (#351)
    * fix: update story
    
    * fix: render menu item parent in accordion
    
    * fix: remove y-spacing between accordion items

--- src/components/ZNavBar/ZNavBar.vue
+++ src/components/ZNavBar/ZNavBar.vue
@@ -99,11 +99,9 @@ export default {
               accordionItems = h(
                 'div',
                 {
-                  class: 'flex flex-col space-y-1 text-base my-1'
+                  class: 'flex flex-col text-base text-vanilla-400 my-1'
                 },
-                items.map((item) => {
-                  return item?.componentOptions?.children
-                })
+                items
               )
             return h(ZAccordionItem, {
               ...options.data,
