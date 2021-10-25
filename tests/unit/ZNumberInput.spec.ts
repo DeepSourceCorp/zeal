3513a80fef0ed93eed31353f9b0876e3dfef52ba
Yash Dave <87606277+yash-deepsource@users.noreply.github.com>
2021-10-25 16:14:37 +0530

    feat: add a number input component to zeal [PLT-3050] (#371)
    * feat: add a number input component to zeal
    
    * Format code with prettier
    
    * fix: deepsource errors
    
    * fix: update snapshots and default value for input
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ tests/unit/ZNumberInput.spec.ts
@@ -0,0 +1,30 @@
+import ZNumberInput from '../../src/components/ZNumberInput'
+
+import { mount } from '@vue/test-utils'
+
+const BasicTab = {
+  template: `
+    <div class="container space-y-5 text-vanilla-100">
+      <z-number-input v-model="inputNum" class="w-28" />
+      <div class="mt-2">Number: {{ inputNum }}</div>
+    </div>
+  `,
+  components: {
+    ZNumberInput
+  },
+  data() {
+    return {
+      inputNum: 0
+    }
+  }
+}
+
+describe('ZNumberInput', () => {
+  it('renders a number input and two buttons', () => {
+    const wrapper = mount(BasicTab)
+
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.findAll('input').length).toEqual(1)
+    expect(wrapper.findAll('button').length).toEqual(2)
+  })
+})
