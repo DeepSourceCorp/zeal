741c9fd343ea26ccf93a1b2adceb536c21d20669
james-deepsource <james@deepsource.io>
2021-12-24 10:19:55 +0530

    feat: mobile layout

--- src/components/ZAlert/ZAlert.vue
+++ src/components/ZAlert/ZAlert.vue
@@ -1,31 +1,57 @@
 <template>
-  <div
-    v-if="isVisible"
-    class="rounded-md bg-opacity-10"
-    :class="[bgColor, borderClass, sourceCodeMarkup ? 'px-4 py-2' : '']"
-  >
+  <div v-if="isVisible">
+    <!-- Mobile layout -->
     <div
-      class="flex items-center justify-between"
-      :class="[sourceCodeMarkup ? '' : 'px-4 py-2']"
-      :style="!sourceCodeMarkup && 'min-height: 49px'"
+      class="flex md:hidden flex-row justify-between rounded-md bg-opacity-10 px-4 py-2"
+      :class="[bgColor, borderClass]"
     >
-      <p class="text-sm" :class="textColor">
-        <slot />
-      </p>
+      <div :class="{ 'space-y-3': $slots.controls || sourceCodeMarkup }">
+        <p class="text-sm" :class="textColor">
+          <slot />
+        </p>
 
-      <div class="flex items-center space-x-2">
-        <div class="inline-flex space-x-2">
+        <div v-if="sourceCodeMarkup">
+          <z-code :content="sourceCodeMarkup" />
+        </div>
+
+        <div>
           <slot name="controls" />
         </div>
+      </div>
 
-        <div v-if="dismissible">
-          <z-icon icon="x" :color="colors[type]" size="small" class="cursor-pointer" @click="isVisible = false" />
-        </div>
+      <div v-if="dismissible">
+        <z-icon icon="x" :color="colors[type]" size="small" class="cursor-pointer" @click="isVisible = false" />
       </div>
     </div>
 
-    <div v-if="sourceCodeMarkup">
-      <z-code :content="sourceCodeMarkup" />
+    <!-- Medium screens and above -->
+    <div
+      class="hidden md:block rounded-md bg-opacity-10"
+      :class="[bgColor, borderClass, sourceCodeMarkup ? 'px-4 py-2' : '']"
+    >
+      <div
+        class="flex items-center justify-between"
+        :class="[sourceCodeMarkup ? '' : 'px-4 py-2']"
+        :style="!sourceCodeMarkup && 'min-height: 49px'"
+      >
+        <p class="text-sm" :class="textColor">
+          <slot />
+        </p>
+
+        <div class="flex items-center space-x-2">
+          <div class="inline-flex space-x-2">
+            <slot name="controls" />
+          </div>
+
+          <div v-if="dismissible">
+            <z-icon icon="x" :color="colors[type]" size="small" class="cursor-pointer" @click="isVisible = false" />
+          </div>
+        </div>
+      </div>
+
+      <div v-if="sourceCodeMarkup">
+        <z-code :content="sourceCodeMarkup" />
+      </div>
     </div>
   </div>
 </template>
