86d08ae6d7e90d87855b087a688eccdbeb223bde
Rohan <rohan@deepsource.io>
2022-01-21 12:20:13 +0530

    feat: add story for z-menu hover variant

--- src/components/ZMenu/ZMenu.stories.ts
+++ src/components/ZMenu/ZMenu.stories.ts
@@ -45,6 +45,35 @@ export const DefaultMenu = () => ({
   }
 })
 
+export const MenuWithHoverTrigger = () => ({
+  components: { ZMenu, ZMenuItem, ZMenuSection, ZDivider },
+  template: `
+    <div class="container">
+        <z-menu :triggerOnHover="true">
+          <template slot="body">
+            <z-menu-section title="Logged In As">
+              <z-menu-item>evan@deepsource.io</z-menu-item>
+            </z-menu-section>
+            <z-menu-section title="Actions">
+              <z-menu-item :action="goToSettings">Account Settings</z-menu-item>
+              <z-menu-item :action="goToSettings" :disabled="true">Autofix (Coming Soon)</z-menu-item>
+              <z-menu-item :action="goToSettings">Contact Support</z-menu-item>
+              <z-menu-item :action="goToSettings">Give Feedback</z-menu-item>
+            </z-menu-section>
+            <z-menu-section :divider="false">
+              <z-menu-item :action="goToSettings">Switch User</z-menu-item>
+              <z-menu-item :action="goToSettings">Sign out</z-menu-item>
+            </z-menu-section>
+          </template>
+        </z-menu>
+    </div>`,
+  methods: {
+    goToSettings() {
+      alert('Action Triggered')
+    }
+  }
+})
+
 export const MultipleSizes = () => ({
   components: { ZMenu, ZMenuItem, ZMenuSection, ZDivider, ZButton },
   template: `
