1d7cbd12d50d76c54a835accf3efe786f11ec601
James George <james@deepsource.io>
2022-06-09 18:08:31 +0530

    feat: add the ability to specify a custom border width (#447)

--- src/components/ZTimeline/ZTimeline.stories.ts
+++ src/components/ZTimeline/ZTimeline.stories.ts
@@ -62,3 +62,43 @@ export const TimelineWithCustomIcon = () => ({
     </z-timeline>
     </div>`
 })
+
+export const TimelineWithCustomBorderWidth = () => ({
+  components: { ZTimeline, ZTimelineItem, ZAvatar },
+  template: `<div class='padded-container'>
+    <z-timeline>
+      <z-timeline-item content="Carter Workman" timestamp="Friday, 4:24PM" border-width-class="" class="h-32">
+        <template slot="icon">
+        <z-avatar
+          image="https://randomuser.me/api/portraits/women/24.jpg"
+          user-name="Akshay Paliwal"
+          size="sm"
+        ></z-avatar></template>
+      </z-timeline-item>
+      <z-timeline-item content="Carter Workman" timestamp="Friday, 4:24PM" border-width-class="" class="h-32">
+        <template slot="icon">
+        <z-avatar
+          image="https://randomuser.me/api/portraits/women/24.jpg"
+          user-name="Akshay Paliwal"
+          size="sm"
+        ></z-avatar></template>
+      </z-timeline-item>
+      <z-timeline-item content="Carter Workman" timestamp="Friday, 4:24PM" border-width-class="" class="h-32">
+        <template slot="icon">
+        <z-avatar
+          image="https://randomuser.me/api/portraits/women/24.jpg"
+          user-name="Akshay Paliwal"
+          size="sm"
+        ></z-avatar></template>
+      </z-timeline-item>
+      <z-timeline-item content="Carter Workman" timestamp="Friday, 4:24PM" border-width-class="" class="h-32">
+        <template slot="icon">
+        <z-avatar
+          image="https://randomuser.me/api/portraits/women/24.jpg"
+          user-name="Akshay Paliwal"
+          size="sm"
+        ></z-avatar></template>
+      </z-timeline-item>
+    </z-timeline>
+    </div>`
+})
