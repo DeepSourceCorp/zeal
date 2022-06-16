04fdd4e478a799784f45cf7eb2cb5b319e3da4b6
james-deepsource <james@deepsource.io>
2022-06-16 13:14:19 +0530

    chore: adjust addon font size and line height based on ZInput

--- src/components/ZInputGroup/ZInputGroup.vue
+++ src/components/ZInputGroup/ZInputGroup.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex">
-    <div class="flex items-center p-2 rounded-tl-sm rounded-bl-sm outline-none" :class="[addonBgColor, addonHeight]">
+    <div class="flex items-center p-2 rounded-tl-sm rounded-bl-sm outline-none" :class="[addonBgColor, addonClasses]">
       <span class="whitespace-nowrap">
         <slot name="addon">
           {{ prefix }}
         </slot>
       </span>
     </div>
-    <z-input v-bind="$attrs" class="rounded-tl-none rounded-bl-none" @on="$listeners" />
+    <z-input v-bind="$attrs" class="border-l-0 rounded-tl-none rounded-bl-none" @on="$listeners" />
   </div>
 </template>
 
@@ -32,17 +32,17 @@ export default Vue.extend({
     }
   },
   computed: {
-    addonHeight(): string {
-      const map = {
-        'x-small': 'h-6',
-        small: 'h-8',
-        medium: 'h-10',
-        large: 'h-12',
-        'x-large': 'h-13',
-        xlarge: 'h-13'
+    addonClasses(): string {
+      const classMap = {
+        'x-small': 'h-6 text-xs leading-loose',
+        small: 'h-8 text-xs leading-loose',
+        medium: 'h-10 text-sm leading-8',
+        large: 'h-12 text-base leading-9',
+        'x-large': 'h-13 text-lg leading-9',
+        xlarge: 'h-13 text-lg leading-9'
       }
 
-      return map[this.$attrs.size || 'medium']
+      return classMap[this.$attrs.size || 'medium']
     }
   }
 })
