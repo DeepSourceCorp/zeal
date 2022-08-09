b07c84376f568be000710625e193ce37aff0556f
rohan-deepsource <rohan@deepsource.io>
2022-08-09 15:29:45 +0530

    feat: add story to showcase updates of page prop

--- src/components/ZPagination/ZPagination.stories.ts
+++ src/components/ZPagination/ZPagination.stories.ts
@@ -70,3 +70,24 @@ export const SmallWindows = () => ({
       ></z-pagination>
     </div>`
 })
+
+export const UpdateFromParent = () => ({
+  components: { ZPagination },
+  data() {
+    return {
+      currentPage: 5
+    }
+  },
+  template: `
+    <div class="container space-y-5 p-3">
+      <z-pagination
+        :page="currentPage"
+        :totalPages="12"
+        :totalVisible="5"
+      ></z-pagination>
+      <div class="flex gap-x-4 text-vanilla-400">
+        <button class="py-1 px-2 border border-ink-100 rounded-md bg-ink-300 hover:bg-ink-200 focus:outline-none" @click="() => {if (this.currentPage > 1) this.currentPage -= 1}">Previous</button>  
+        <button class="py-1 px-2 border border-ink-100 rounded-md bg-ink-300 hover:bg-ink-200 focus:outline-none" @click="() => {if (this.currentPage !== 12) this.currentPage += 1}">Next</button>
+      </div>
+    </div>`
+})
