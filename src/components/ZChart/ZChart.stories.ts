ad0f58f8bb56359208c96578ec1bfdbc62222653
Shivam Mishra <shivam@deepsource.io>
2022-11-15 12:32:34 +0530

    feat: add format tooltip example

--- src/components/ZChart/ZChart.stories.ts
+++ src/components/ZChart/ZChart.stories.ts
@@ -112,3 +112,39 @@ export const PercentageChart = () => ({
     <z-chart :dataSets="dataSets" :labels="labels" :yRegions=yRegions type="percentage"></z-chart>
   </div>`
 })
+
+interface ChartDatasetSet {
+  name: string
+  index: number
+  values: Array<number>
+}
+
+export const AxisMixedChart = () => ({
+  components: { ZChart },
+  data() {
+    return {
+      dataSets: [
+        { name: 'ACTIVE ISSUES', chartType: 'bar', values: [17, 19, 12, 9, 14, 5, 5, 4] },
+        { name: 'RESOLVED ISSUES', chartType: 'bar', values: [-17, -15, -19, -11, -12, 0, -4, -2] },
+        { name: 'NET NEW ISSUES', chartType: 'line', values: [0, 4, -7, -2, 2, 5, 1, 2] }
+      ],
+      labels: issueData.labels,
+      tooltipOptions: {
+        formatTooltipY: (d: number, set: ChartDatasetSet) => {
+          const formattedValue = set.index === 1 ? Math.abs(d) : d
+          
+          if (set.index === 2) {
+            return `<span style="color: ${formattedValue > 0 ? '#33CB9A' : '#E17783'}">${formattedValue}</span>`
+          }
+          return formattedValue
+        },
+        formatTooltipX: (label: string) => {
+          return `Issues stats for ${label}`
+        }
+      }
+    }
+  },
+  template: `<div class="padding-container py-20">
+    <z-chart :dataSets="dataSets" :labels="labels" :tooltipOptions="tooltipOptions" type="axis-mixed"></z-chart>
+  </div>`
+})
