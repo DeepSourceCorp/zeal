3687b70517b75c9c940bddd85e744bb704d94acb
james-deepsource <james@deepsource.io>
2023-03-17 13:24:25 +0530

    feat(ZSelect): add maxHeight prop

--- src/components/ZSelect/ZSelect.vue
+++ src/components/ZSelect/ZSelect.vue
@@ -44,8 +44,8 @@
     </div>
     <!-- prettier-ignore -->
     <div
-      class="absolute left-0 right-0 z-10 mt-1 overflow-y-auto transition-all duration-300 border border-solid rounded-md max-h-64 options shadow-black border-ink-100 text-vanilla-300 bg-ink-300"
-      :class="!open && 'hidden'"
+      class="absolute left-0 right-0 z-10 mt-1 overflow-y-auto transition-all duration-300 border border-solid rounded-md options shadow-black border-ink-100 text-vanilla-300 bg-ink-300"
+      :class="[ { 'hidden' : !open  }, maxHeight  ]"
     >
       <slot></slot>
     </div>
@@ -115,7 +115,11 @@ export default Vue.extend({
     truncate: {
       default: false,
       type: Boolean
-    }
+    },
+    maxHeight: {
+      type: String,
+      default: 'max-h-64'
+    },
   },
 
   model: {
