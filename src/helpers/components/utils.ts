1d94ece6b38030b8f5f9ed15446e044c55d39afe
Yash Dave <yash@deepsource.io>
2023-06-21 18:36:14 +0530

    docs: add JSDocs for utils

--- src/helpers/components/utils.ts
+++ src/helpers/components/utils.ts
@@ -5,13 +5,26 @@ export const toPascal = (word: string) => {
     .replace(new RegExp(/[^\w\s]/, 'g'), '')
     .replace(new RegExp(/\s+(.)(\w+)/, 'g'), ($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`)
     .replace(new RegExp(/\s/, 'g'), '')
-    .replace(new RegExp(/\w/), (s) => s.toUpperCase())
+    .replace(new RegExp(/\w/), s => s.toUpperCase())
 }
 
+/**
+ * Function that returns `true` or `false` depending on whether the `target` was found in `parentCandidate` or not
+ *
+ * @param parentCandidate HTMLElement reference of the parent element
+ * @param target HTMLElement reference of the element to find inside parent element
+ * @returns `true` or `false` depending on whether the `target` was found in `parentCandidate` or not
+ */
 export const containsElement = (parentCandidate: HTMLElement, target: HTMLElement): boolean => {
   return Boolean(parentCandidate && (target === parentCandidate || parentCandidate.contains(target)))
 }
 
+/**
+ *  Shorten long numbers into k, m, b, and t notation
+ *
+ * @param candidate - A number or string value to be shortened
+ * @returns A number or string value depending on whether the number was shortened or not
+ */
 export const shortenLargeNumber = (candidate: number | string): number | string => {
   let number = 0
 
